3628d9df30e44736fefe8e71de7398a53bb6b475
Guido Knips <Guido.Knips@googlemail.com>
2016-08-04 21:54:02 +0200

    spawn improved workers if possible

--- src/spawn.creeps.js
+++ src/spawn.creeps.js
@@ -15,6 +15,18 @@ const countCreepsByRole = function(creeps) {
     }, {});
 };
 
+const getRoomSpawnEnergyAndCapacity = function(room) {
+  const spawnsAndExtensions = room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN);
+    }
+  });
+
+  return _.reduce(spawnsAndExtensions, (sum, item) => {
+    return [sum[0] + item.energy, sum[1] + item.energyCapacity];
+  }, [0, 0]);
+};
+
 const buryCreeps = function(game, memory) {
     for(var name in memory.creeps) {
         if(!game.creeps[name]) {
@@ -24,19 +36,35 @@ const buryCreeps = function(game, memory) {
     }
 };
 
+const BASIC_WORKER = [WORK, CARRY, MOVE]; // cost: 200
+const ADVANCED_WORKER = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]; // cost: 500
+
 const spawnCreepsAsNecessary = function(creeps, spawn) {
     const creepCount = countCreepsByRole(creeps);
+    const [energy, capacity] = getRoomSpawnEnergyAndCapacity(spawn.room);
+
+    // Build bigger workers if possible
+    let workerTemplate = BASIC_WORKER;
+    if (capacity >= 500) {
+      workerTemplate = ADVANCED_WORKER;
+    }
+
     if (!(roles.HARVESTER in creepCount) || creepCount[roles.HARVESTER] < 1) {
-        var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.HARVESTER});
+        // if we do not have any harvesters, and not enough to build a big one, build a small one to get things going.
+        let harvesterTemplate = workerTemplate;
+        if (energy < 500 && !creepCount[roles.HARVESTER]) {
+          harvesterTemplate = BASIC_WORKER;
+        }
+        var newName = spawn.createCreep(workerTemplate, undefined, {role: roles.HARVESTER});
         console.log("Spawned new harvester: " + newName);
     }
     if (!(roles.UPGRADER in creepCount) || creepCount[roles.UPGRADER] < 2) {
-        var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.UPGRADER});
+        var newName = spawn.createCreep(workerTemplate, undefined, {role: roles.UPGRADER});
         console.log("Spawned new upgrader: " + newName);
     }
     if (!(roles.BUILDER in creepCount) || creepCount[roles.BUILDER] < 2) {
         if (spawn.room.find(FIND_CONSTRUCTION_SITES)) {
-            var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.BUILDER});
+            var newName = spawn.createCreep(workerTemplate, undefined, {role: roles.BUILDER});
             console.log("Spawned new builder: " + newName);
         }
     }
