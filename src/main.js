c7d57831af2a13c0f8a0491a775a913193711bec
Guido Knips <Guido.Knips@googlemail.com>
2016-08-04 20:18:08 +0200

    Creep-Spawning umorganisiert

--- src/main.js
+++ src/main.js
@@ -12,26 +12,7 @@ roleBehaviors[roles.BUILDER] = roleBuilder;
 roleBehaviors[roles.AATTACKER] = roleAttacker;
 
 module.exports.loop = function () {
-    creepSpawner.buryCreeps();
-    const creepCount = creepSpawner.countCreepsByRole(Game.creeps);
-    if (!(roles.HARVESTER in creepCount) || creepCount[roles.HARVESTER] < 1) {
-        var newName = Game.spawns['Nest'].createCreep([WORK,CARRY,MOVE], undefined, {role: roles.HARVESTER});
-        console.log("Spawned new harvester: " + newName);
-    }
-    if (!(roles.UPGRADER in creepCount) || creepCount[roles.UPGRADER] < 2) {
-        var newName = Game.spawns['Nest'].createCreep([WORK,CARRY,MOVE], undefined, {role: roles.UPGRADER});
-        console.log("Spawned new upgrader: " + newName);
-    }
-    if (!(roles.BUILDER in creepCount) || creepCount[roles.BUILDER] < 2) {
-        if (Game.spawns['Nest'].room.find(FIND_CONSTRUCTION_SITES)) {
-            var newName = Game.spawns['Nest'].createCreep([WORK,CARRY,MOVE], undefined, {role: roles.BUILDER});
-            console.log("Spawned new builder: " + newName);
-        }
-    }
-    if (!(roles.ATTACKER in creepCount) || creepCount[roles.Attacker] < 1) {
-        var newName = Game.spawns['Nest'].createCreep([ATTACK, ATTACK,MOVE, MOVE], undefined, {role: roles.ATTACKER});
-        console.log("Spawned new attacker: " + newName);
-    }
+    creepSpawner.manageCreeps(Game, Memory);
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
--- src/spawn.creeps.js
+++ src/spawn.creeps.js
@@ -1,3 +1,5 @@
+const roles = require('roles');
+
 const countCreepsByRole = function(creeps) {
     return _.reduce(_.values(creeps), (counter, creep) => {
         const role = creep.memory.role;
@@ -13,16 +15,44 @@ const countCreepsByRole = function(creeps) {
     }, {});
 };
 
-const buryCreeps = function() {
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
+const buryCreeps = function(game, memory) {
+    for(var name in memory.creeps) {
+        if(!game.creeps[name]) {
+            delete memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
     }
 };
 
+const spawnCreepsAsNecessary(creeps, spawn) {
+    const creepCount = creepSpawner.countCreepsByRole(creeps);
+    if (!(roles.HARVESTER in creepCount) || creepCount[roles.HARVESTER] < 1) {
+        var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.HARVESTER});
+        console.log("Spawned new harvester: " + newName);
+    }
+    if (!(roles.UPGRADER in creepCount) || creepCount[roles.UPGRADER] < 2) {
+        var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.UPGRADER});
+        console.log("Spawned new upgrader: " + newName);
+    }
+    if (!(roles.BUILDER in creepCount) || creepCount[roles.BUILDER] < 2) {
+        if (spawn.room.find(FIND_CONSTRUCTION_SITES)) {
+            var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: roles.BUILDER});
+            console.log("Spawned new builder: " + newName);
+        }
+    }
+    if (!(roles.ATTACKER in creepCount) || creepCount[roles.ATTACKER] < 1) {
+        var newName = spawn.createCreep([ATTACK, ATTACK,MOVE, MOVE], undefined, {role: roles.ATTACKER});
+        console.log("Spawned new attacker: " + newName);
+    }
+
+}
+
+const manageCreeps(game, memory) {
+  buryCreeps(game, memory);
+  spawnCreepsAsNecessary(game.creeps, Game.spawns['Nest']);
+}
+
 module.exports = {
     countCreepsByRole: countCreepsByRole,
-    buryCreeps: buryCreeps
+    manageCreeps: manageCreeps
 };
