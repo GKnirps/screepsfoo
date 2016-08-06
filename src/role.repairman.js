9350fe37da08485757c4689c2d2bd7f6113d822a
Guido Knips <Guido.Knips@googlemail.com>
2016-08-06 22:40:55 +0200

    repairman now memorizes its target

--- src/role.repairman.js
+++ src/role.repairman.js
@@ -1,10 +1,36 @@
 const findHelpers = require('helpers.find');
 const common = require('common_behaviors');
 
+const isValidRepairTarget = function(structure) {
+  if (!structure.my && structure.structureType !== STRUCTURE_ROAD && structure.structureType !== STRUCTURE_WALL) {
+    return false;
+  }
+  if (structure.structureType === STRUCTURE_WALL && structure.structureType === STRUCTURE_RAMPART) {
+    if (structure.hits > 100) {
+      return false;
+    }
+  }
+  return structure.hits < structure.hitsMax;
+};
+
+const getStructureToRepair = function(creep) {
+  if (creep.memory.repairTargetId) {
+    previousTarget = Game.getObjectById(creep.memory.repairTargetId);
+    if (isValidRepairTarget(previousTarget) {
+      return previousTarget;
+    }
+  }
+  const structures = creep.room.find(FIND_STRUCTURES, {filter: isValidRepairTarget});
+  if (!structures.length) {
+    return null;
+  }
+  return findHelpers.getClosestObjectFromList(creep.pos, structures);
+};
+
 /**
  * Creep that repairs structures, but does not boost walls and Ramparts above 100
  */
-var roleRepairman = {
+const roleRepairman = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -16,22 +42,12 @@ var roleRepairman = {
             creep.memory.repairing = true;
             creep.say('repairing');
         }
-        const structures = creep.room.find(FIND_STRUCTURES, {filter: structure => {
-          if (!structure.my && structure.structureType !== STRUCTURE_ROAD && structure.structureType !== STRUCTURE_WALL) {
-            return false;
-          }
-          if (structure.structureType === STRUCTURE_WALL && structure.structureType === STRUCTURE_RAMPART) {
-            if (structure.hits > 100) {
-              return false;
-            }
-          }
-          return structure.hits < structure.hitsMax;;
-        }});
-        if (!structures.length) {
+        const structure = getStructureToRepair(creep);
+        if (!structure) {
             // TODO: we can do wall improving if nothing is to be repaired (when we have a wall improver)
             creep.moveTo(Game.spawns['Nest']);
         } else {
-            const structure = findHelpers.getClosestObjectFromList(creep.pos, structures);
+            creep.memory.repairTargetId = structure.id; 
             if(creep.memory.repairing) {
                 if(creep.repair(structure) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(structure);
